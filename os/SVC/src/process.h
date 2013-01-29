af77127d40373a2e0a44c0619eeb58a2cdcc9459
Desmond Chan <dezmo1992@hotmail.com>
2013-01-28 23:41:51 -0500

    Adding BST data structure for process control blocks

--- os/SVC/src/process.h
+++ os/SVC/src/process.h
@@ -3,8 +3,51 @@
 
 typedef enum {NEW, READY, RUN} process_state;
 typedef struct {
+    int pid;        // process id
+    int priority;   // priority. Ranges from values 0-4
+    uint32_t *sp;   // Stack pointer
 
 } pcb;
+
+// BST used to store the process ids along with their priorities
+typedef struct{
+    pcb *root;
+    processBST *left;
+    processBST *right;
+} processBST;
+
+/*************************************************************************
+ *          PCB BST DEFINITIONS
+ *************************************************************************/
+void processBST_init(processBST& root){
+    root = NULL;
+}
+
+processBST processBST_lookup_by_pid(int pid, processBST& root){
+    if(root == NULL){
+        return NULL;
+    }
+    else if (pid == root->pcb->pid){
+        return root;
+    }
+    else if(pid < root->pcb->pid){
+        return processBST_lookup_by_pid(pid, root->left);
+    }
+    else{
+        return processBST_lookup_by_pid(pid, root->right);
+    }
+}
+// Priority lookup based on process id
+int processBST_priority_lookup(int pid, processBST& root){
+    processBST *node = processBST_lookup_by_pid(pid, root);
+
+    // As per section 3.5 of project description, if pid is invalid return "-1"
+    if(node == NULL){
+        return -1;
+    }
+    return node->pcb->priority;
+}
+
 
 
 pcb* current_processor;
@@ -14,15 +57,18 @@ void process_init() {
 }
 
 int set_process_priority(int pid, int priority) {
-    pcb process = pcbLookup(pid);
+    processBST *node = processBST_lookup_by_pid(pid, root);
 
-    process.priority = priority;
+    // As per project description section 2.4, if no process exists with the pid passed in return a non-zero int value
+    if (node == NULL){
+        return -1;
+    }
+    node->pcb->priority = priority;
+    return 0;
 }
 
 int get_process_priority(int pid) {
-    pcb process = pcbLookup(pid);
-
-    return process.priority;
+    return processBST_priority_lookup(pid, root);
 }
 
 int context_switch(pcb& proc) {
@@ -33,11 +79,11 @@ int scheduler() {
 
 }
 
-void processSwitch(){
+void process_switch(){
     // Go through process queues that are 'ready' and take the highest priority process
 
     // If process queue is empty execute the null process
-    null_process();
+
 }
 
 int release_processor() {
