1eac641b2ff52164b39b7578f3489afe5dfdf54b
Bill He <hezhenqian@gmail.com>
2013-01-28 23:51:46 -0500

    modified pqueue stuff

--- os/SVC/src/pqueue.h
+++ os/SVC/src/pqueue.h
@@ -1,3 +1,7 @@
+#ifndef NULL
+#define NULL ((void *)0) 
+#endif
+
 #ifndef PROCESS_H
 #define PROCESS_H
 
@@ -6,12 +10,8 @@ extern enum proc_state_t;
 
 typedef struct {
 	proc_state_t state;
-	pcb *pq_array[4];
+	pcb *pq_end[4];
+	pcb *pq_front[4];
 } pqueue;
 
-pcb* pqueue_dequeue(pqueue *queue, int priority)
-{
-	
-}
-bool 
 #endif
