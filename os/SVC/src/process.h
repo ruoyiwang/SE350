4e4dcdd9331273e5606ff247e11acc7e2c0bcc7e
David Guo <david.guo@uwaterloo.ca>
2013-01-28 22:53:31 -0500

    Checkin process and pqueue.h

--- os/SVC/src/process.h
+++ os/SVC/src/process.h
@@ -7,7 +7,7 @@ typedef struct {
 } pcb;
 
 
-pcb* current_processor;
+pcb* current_process;
 
 void process_init() {
 
@@ -21,18 +21,25 @@ int get_process_priority(int pid) {
 	
 }
 
-int context_switch(pcb& proc) {
 
-}
-
-int process_switch() {
-
-}
-
-int release_processor() {
-	current_processor->state = READY;
+// Return 0 if success; 1 if fail
+int context_switch() {
+	current_process->state = READY;
 	rq_enqueue(current_processor);
-	process_switch();
+	
+}
+
+// Return 0 if success; 1 if fail
+int release_processor() {
+	if (current_processor = NULL) {
+		return 1;
+	}
+
+	if (process_switch()) {
+		return 1;
+	}
+
+	return 0;
 }
 
 #endif
