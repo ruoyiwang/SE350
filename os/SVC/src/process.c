938a5f7ad77b3a0c204ded27d7feb409697d2979
Bill He <hezhenqian@gmail.com>
2013-01-30 16:25:15 -0500

    changed some priorities

--- os/SVC/src/process.c
+++ os/SVC/src/process.c
@@ -102,8 +102,8 @@ void process_init() {
 	{
 		/* initialize the first process	exception stack frame */
 		pcbs[i].pid = i;
-		pcbs[i].state = READY;
-		pcbs[i].priority = 3;
+		pcbs[i].state = NEW;
+		pcbs[i].priority = 2;
 
 		sp  = request_memory_block();
 			
@@ -123,6 +123,7 @@ void process_init() {
 		//pcb_insert(&pcbs[i], root);
 	}
 
+	pcbs[0].priority = 3;
 	process_switch();
 }
 
