c4ccabcb7711c14d998b5faf3077737537bf170f
Bill He <hezhenqian@gmail.com>
2013-01-30 01:06:22 -0500

    fixing memory compile errors

--- os/SVC/src/memory.h
+++ os/SVC/src/memory.h
@@ -42,8 +42,8 @@ void list_create_node_and_push(list *q, int a){
 }
 
 void list_push(list *q, lnode *node){
-	q->end->next = curNode;
-	q->end = curNode;
+	q->end->next = node;
+	q->end = node;
 	q->size++;
 }
 
@@ -59,7 +59,6 @@ lnode* list_pop(list *q){
 }
 
 MMU* mmu_create_new_mmu(){
-	int *temp, i;
 	lnode* curNode;
 	//initialize all vars
 	MMU* memory_management_unit;
@@ -71,7 +70,7 @@ MMU* mmu_create_new_mmu(){
 	memory_management_unit->memory_not_in_use.front = curNode;
 	memory_management_unit->memory_not_in_use.end = curNode;
 
-	for (i; curNode->address != memory_management_unit->free_mem; curNode->address -= USR_SZ_STACK){
+	for (; curNode->address != memory_management_unit->free_mem; curNode->address -= USR_SZ_STACK){
 		list_create_node_and_push(&memory_management_unit->memory_not_in_use, curNode->address);
 	}
 
@@ -98,17 +97,18 @@ void * request_memory_block(){
 }
 
 int release_memory_block(void *MemoryBlock){
-	lnode* curNode, prevNode;
+	lnode* curNode, *prevNode;
 
 	curNode = mmu->memory_in_use.front;
 
 	//edge case where the node is at the beginning of the list
 	if ((int)MemoryBlock == curNode->address){
-		list_pop(&mmu->memory_in_use)
+		list_pop(&mmu->memory_in_use);
 		list_push(&mmu->memory_not_in_use, curNode);
 		return 0;
 	}
-
+	prevNode = curNode;
+	curNode = curNode->next;
 	while (curNode!=mmu->memory_in_use.end){
 		if ((int)MemoryBlock==curNode->address){
 			//if found
