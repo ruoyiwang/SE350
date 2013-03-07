3eb4dc68fcaf3f2f3b2fc0a372bd5baf57060752
ruoyiwang <wang.ruoyi.uw@gmail.com>
2013-03-07 01:02:40 -0500

    almost finalizing the CRT wrapper thingy

---
+++ os/SVC/src/CRT.h
@@ -0,0 +1,31 @@
+#include "process.h"
+#include "memory.h"
+#include <LPC17xx.h>
+#include "uart_polling.h"
+
+#ifndef __CRT_DISPLAY_PROCESS__
+#define __CRT_DISPLAY_PROCESS__
+void crt_displpay_process(){
+	envelope * message_envelop = null;
+	message_envelop = receive_message();
+	while(message_envelop != null){
+		//pretty much as long as I get messages, I do this shit
+		//will only care if the message is a display request, or else I just delete the message wihtout handling it
+		if (message_envelop->message_type == DISPLAY_REQUEST){
+			//handle the message
+			//notice here I am actually just changing the msg info and gonna pass it on anyways
+			message_envelop->src_id = current_process->pid;
+			message_envelop->dest_id = "i-process' pid";
+			//message_envelop->message_type = "some message type needed";
+			//for now use the same request
+		
+			//send message to i-process that contains the message and shit
+			send_message(message_envelop->dest_id, message_envelop);
+		}
+
+		release_memory_block(message_envelop);
+		message_envelop = receive_message();		
+	}
+}
+
+#endif 
