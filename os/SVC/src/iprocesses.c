8d8313173bfea7a1ef717f05872eeb72d49dd393
Desmond Chan <dezmo1992@hotmail.com>
2013-03-08 18:08:58 -0500

    Copy char buffer to a char for message sending to kcb. TODO: change the dest_id of the message from the var kcb_proc_id to an actual id (we havent assigned a pid to kcb yet)

--- os/SVC/src/iprocesses.c
+++ os/SVC/src/iprocesses.c
@@ -12,18 +12,30 @@
 int display_message_ready;
 
 void i_process_routine(void){
+	char* char_buffer_string;
 	// make an empty envelope to for the crt msgs
 	envelope* crt_message = NULL;
+	LPC_UART_TypeDef *pUart = (LPC_UART_TypeDef *)LPC_UART0;
+
 	// Create an envelope for the kcd message send
 	envelope* kcd_command = k_request_memory_block();
-	kcd_command->message = g_UART0_buffer[0];
 	kcd_command->src_id = interrupt_process->pcb->pid;
 	kcd_command->dest_id = kcd_process_id;
 
+	// Make sure that interrupts don't add to the char buffer
+	// Disable the RBR in the IER
+	pUart->IER = pUart->IER & 0xfffffff8;
+
 	// If the first char in the buffer is 0x25 i.e '%', then we send a command registration
 	if(g_UART0_buffer[0] == 0x25){
+		for(int i = 0 ; i < g_UART0_count; i++){
+			*(char+i) = g_UART0_buffer[i];
+			g_UART0_buffer[i] = 0;
+		}
+		g_UART0_count = 0;
 		// Code for sending a message to the KCD for a command registration
 		kcd_command->type = COMMAND_REGISTRATION;
+		kcd_command->message = char_buffer_string;
 		send_message(kcd_command->dest_id, kcd_command);
 	}	
 	else if(display_message_ready == 1){		//if there's a message ready for me to print to CRT
@@ -45,10 +57,20 @@ void i_process_routine(void){
 	}
 	//else we know that we send a keyboard input
 	else{
+		for(int i = 0 ; i < g_UART0_count; i++){
+			*(char+i) = g_UART0_buffer[i];
+			g_UART0_buffer[i] = 0;
+		}
+		g_UART0_count = 0;
 		// Code for sending a message to the KCD for a command registration
 		kcd_command->type = KEYBOARD_INPUT;
+		kcd_command->message = char_buffer_string;
 		send_message(kcd_command->dest_id, kcd_command);
 	}	
+
+	// Since we are done with the interrupt we can reenable RBR
+	pUart->IER = pUart->IER | 0x7; 
+
 }
 
 void uart_send_string( uint32_t n_uart, uint8_t *p_buffer, uint32_t len )
