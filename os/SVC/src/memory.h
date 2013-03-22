6b26c00a3d71f20f8d2663724b27df3276a5cdf3
Ruoyi Wang <wang.ruoyi.uw@gmail.com>
2013-03-22 00:21:01 -0400

    adding timer reserved memory logic, adding corresponding externs for timer reserved mem, irq reserved mem

--- os/SVC/src/memory.h
+++ os/SVC/src/memory.h
@@ -16,7 +16,7 @@ typedef struct mmu_t{
 	char lookup_table[256];
 	unsigned int free_mem;
 	unsigned int max_mem;
-	char irq_using_mem1, irq_using_mem2;
+	char irq_using_mem1, irq_using_mem2, timer_using_mem;
 	unsigned int lookup_table_size;
 	unsigned int actual_size;
 	char memory_available;	//1 is there is avail mem, 0 is there isn't
@@ -38,6 +38,13 @@ extern int k_release_memory_block(void *);
 extern int _release_memory_block(U32 p_func, void *MemoryBlock) __SVC_0;
 //int __svc_indirect(0) release_memory_block(void *MemoryBlock);
 
+extern void* k_request_irq_memory_block(void);
+#define request_irq_memory_block() _request_irq_memory_block((U32)k_request_irq_memory_block)
+extern void* _request_irq_memory_block(U32 p_func) __SVC_0;
+
+extern void* k_request_timer_memory_block(void);
+#define request_timer_memory_block() _request_timer_memory_block((U32)k_request_timer_memory_block)
+extern void* _request_timer_memory_block(U32 p_func) __SVC_0;
 //lookup_table - actual address map
 //    0    0x10008000
 //    1    0x10008000 - 128 * 1
